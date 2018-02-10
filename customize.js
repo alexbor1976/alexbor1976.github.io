d7c9d91edd4d833e6c85317c8d14d8e7602d87e6
alexbor1976 <36292241+alexbor1976@users.noreply.github.com>
2018-02-10 11:51:27 +0200

    Add files via upload

---
+++ customize.js
@@ -0,0 +1,49 @@
+// google map initialization
+function initMap(){
+	var eisen_location = {lat: 48.18331, lng: 17.923645};
+	
+	
+	var map = new google.maps.Map(document.getElementById('my_gmap'), {
+		zoom: 12,
+		
+		center: eisen_location,
+		
+		styles: [{ 
+			stylers: [{
+				saturation: -100
+			}]
+		}],
+		
+		disableDefaultUI: true
+	});
+	
+	var marker = new google.maps.Marker({
+	  position: new google.maps.LatLng(eisen_location),
+	  map: map
+	});
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
